cafdbe4786fc3dce360522bd43f6a5f0b768d679
mihir-parikh-11 <mihirparikh113@gmail.com>
2025-08-31 13:47:22 +0530

    Project Setup

---
+++ src/main/java/com/apartment/management/ApartmentManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.apartment.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApartmentManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApartmentManagementApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apartment/management/ApartmentManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apartment.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApartmentManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
