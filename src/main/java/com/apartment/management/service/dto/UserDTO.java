8c9393a02f21e1997f88ba1d22969838145c4a3e
mihir-parikh-11 <mihirparikh113@gmail.com>
2025-09-22 00:10:24 +0530

    feat: AM-28: Profile & User Self-Service

--- src/main/java/com/apartment/management/service/dto/UserDTO.java
+++ src/main/java/com/apartment/management/service/dto/UserDTO.java
@@ -4,6 +4,7 @@ package com.apartment.management.service.dto;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -41,6 +42,7 @@ public class UserDTO implements Serializable {
 
     private ZonedDateTime createdDate;
 
+    @NotNull(message = "Gender cannot be null")
     private GenderDTO gender;
 
     private List<AuthorityRoleDTO> roles;
